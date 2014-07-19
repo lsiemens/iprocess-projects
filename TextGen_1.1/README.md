92e5dd377022315dd24229bf058a5fc6614b67ff
Luke Siemens <lsiemens@users.noreply.github.com>
2014-07-19 12:55:48 -0700

    prepared TextGen for the first oficial release

---
+++ TextGen_1.1/README.md
@@ -0,0 +1,21 @@
+TextGen 1.1
+===========
+
+TextGen is a script to produce random text with statistical properties
+similar to the supplied training text. This Project is described in
+more detail in the blog post [The Making of a Markov Chain]
+(lsiemens.github.io/the-making-of-a-markov-chain/index.html).
+
+The contents of this directory include,
+
+- textGen.py: The python script TextGen, for producing random text with 
+  Markov Chains.
+
+- out.txt: An example output from TextGen.
+
+- prepared_text: The directory containing training text for TextGen
+
+- full_text: The directory containing the full unmodified text used to
+  produce the training text in the directory prepared_text.
+
+- LICENSE: A copy of the BSD-3 license TextGen 1.1 is released under.
--- TextGen_1.1/textGen.py
+++ TextGen_1.1/textGen.py
@@ -1,8 +1,40 @@
+####
+#
+# Copyright (c) 2014, Luke Siemens
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# 1. Redistributions of source code must retain the above copyright
+# notice, this list of conditions and the following disclaimer.
+#
+# 2. Redistributions in binary form must reproduce the above copyright 
+# notice, this list of conditions and the following disclaimer in the 
+# documentation and/or other materials provided with the distribution.
+#
+# 3. Neither the name of the copyright holder nor the names of its 
+# contributors may be used to endorse or promote products derived from
+# this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A 
+# PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT 
+# HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, 
+# SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT 
+# LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, 
+# DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY 
+# THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT 
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE 
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#
+####
+
 """
+TextGen 1.1
 
-text generator 1.1
-
-Text is generated using a markov chain.
+This is a tool to generate random text using a markov chain.
 
 """
 
@@ -100,4 +132,4 @@ text_gen = markov_chain()
 text_gen.calculate_transitions([dir+name+ext for name in names], 5)
 #text_gen.save("transition.data")
 #text_gen.load("transition.data")
-text_gen.generate_string(1000, "he said")
+text_gen.generate_string(2000, "he said")
