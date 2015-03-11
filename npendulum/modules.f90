f7a34093720c8027cd779f9b35c4eca3ee55ab6a
Luke Siemens <lsiemens@users.noreply.github.com>
2015-03-11 11:57:46 -0700

    started working with lapack

--- npendulum/modules.f90
+++ npendulum/modules.f90
@@ -1,8 +1,56 @@
       module modules
         use types, only: rp
         implicit none
-        public :: initalize, zeros
+        public :: theta_dd_solver, initalize, zeros
         contains
+
+        subroutine theta_dd_solver(n, l, g, theta, theta_d, theta_dd)
+          implicit none
+          integer :: i, j, solve_ok
+          integer, allocatable :: pivot(:)
+          real(rp) :: crossterms
+          integer, intent(in) :: n
+          real(rp), intent(in) :: l, g
+          real(rp), intent(in), allocatable :: theta(:), theta_d(:)
+          real(rp), intent(out), allocatable :: theta_dd(:)
+          real(rp), allocatable :: A(:, :), b(:)
+
+          allocate(pivot(n))
+          allocate(A(n, n))
+          allocate(b(n))
+          allocate(theta_dd(n))
+
+          do i = 1, n
+            crossterms = 0.0_rp
+            do j = 1, n
+              crossterms = crossterms + bnij(n, i, j)*l*theta_d(j)*sin(theta(i)-theta(j))
+            end do
+            b(i) = -g*(n+1-i)*sin(theta(i)) - crossterms
+          end do
+
+          do i = 1, n
+            do j = 1, n
+              A(i, j) = bnij(n,i,j)*l*cos(theta(i)-theta(j))
+            end do
+          end do
+
+          print *, A
+          print *, b
+          call sgesv(n, 1, A, n, pivot, b, n, solve_ok)
+
+          if (solve_ok.GT.0) then
+            print *, "The matrix is singular, solution could not be found", solve_ok
+          else if (solve_ok.LT.0) then
+            print *, "The matrix contains illegal values", solve_ok
+          end if
+          theta_dd = b
+        end subroutine theta_dd_solver
+
+        function bnij(n, i, j) result(b)
+          integer, intent(in) :: n, i, j
+          integer :: b
+          b = (n+1-max(i, j))
+        end function bnij
 
         subroutine initalize(file_name, n, l, g, theta, theta_d, theta_dd)
           use pyio, only: openpy, readpy_value, readpy_array, closepy
--- npendulum/npendulum.f90
+++ npendulum/npendulum.f90
@@ -1,15 +1,17 @@
       program npendulum
         use types, only: rp
-        use modules, only: initalize, zeros
+        use modules, only: theta_dd_solver, initalize, zeros
         implicit none
 
         integer :: n, file_id, i, j
         real(rp) :: l, g
         real(rp), allocatable :: theta(:), theta_d(:), theta_dd(:)
-        
         call initalize("initalization.dat", n, l, g, theta, theta_d, theta_dd)
-!        call zeros("initalization.dat", 10)
+!        call zeros("initalization.dat", 2)
         
-        print *, n, l, g
-        print *, theta
+        call theta_dd_solver(n, l, g, theta, theta_d, theta_dd)
+!        print *, theta_dd(:)
+
+!        print *, n, l, g
+        print *, theta_dd
       end program npendulum
