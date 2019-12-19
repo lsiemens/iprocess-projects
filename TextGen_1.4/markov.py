b88e99c2c48bbcceeb2414b5dccc9a8256ba4ef1
Luke Siemens <luke@lsiemens.com>
2019-12-19 02:37:44 -0800

    Change markov.py to encode and decode based on preciese frequency data.

--- TextGen_1.4/markov.py
+++ TextGen_1.4/markov.py
@@ -8,18 +8,19 @@ class Markov:
         self.order = order
         self._training_text = training_text
         self._data = {}
-        self._ordered_symbols = self.symbols
-        self._break_symbol = len(self._ordered_symbols) #give it an index 1 greater than the least common character
 
         self.train()
 
     def train(self):
-        self._data = {symbol:1 for symbol in self._ordered_symbols}
+        self._data = {symbol:1 for symbol in self.symbols} # force every symbol to occure atleast once in the zeroth order chain
         for i in range(len(self._training_text) - self.order):
 
-            if i % ((len(self._training_text) - self.order)//100) == 0:
-                sys.stdout.write("\rMarkov training %d%%" % int(100*i/(len(self._training_text) - self.order)))
-                sys.stdout.flush()
+            try:
+                if i % ((len(self._training_text) - self.order)//100) == 0:
+                    sys.stdout.write("\rMarkov training {0:d}%".format(int(100*i/(len(self._training_text) - self.order))))
+                    sys.stdout.flush()
+            except ZeroDivisionError:
+                pass
 
             substring = self._training_text[i:i + self.order + 1]
             for j in range(self.order + 1):
@@ -28,91 +29,104 @@ class Markov:
                     self._data[key] += 1
                 else:
                     self._data[key] = 1
+        sys.stdout.write("\n")
+        sys.stdout.flush()
+        print(num_found)
 
-        options = self.getOptions()
-        self._ordered_symbols = "".join([key for (key, value) in options])
+#    def getCanonicalText(self, length, seed=""):
+#        sequence = [0]*(length - len(seed))
+#        return self.getText(sequence, seed)
 
-    def getCanonicalText(self, length, seed=""):
-        sequence = [0]*(length - len(seed))
-        return self.getText(sequence, seed)
-
-    def getRandomText(self, length, seed=""):
+#    def getRandomText(self, length, seed=""):
 #        sequence = numpy.random.random_integers(low=0, high=len(self._ordered_symbols) - 1, size=length)
-        sequence = numpy.random.random_integers(low=0, high=1, size=length)
-        print(sequence)
-        return self.getText(sequence, seed)
+#        sequence = numpy.random.random_integers(low=0, high=1, size=length)
+#        print(sequence)
+#        return self.getText(sequence, seed)
 
-    def getText(self, sequence, seed=""):
-        text = seed
-        if self.order > 0:
-            ngram = seed[-self.order:]
-        else:
-            ngram = ""
-        for index in sequence:
-            if index == self._break_symbol:
-                ngram = ngram[1:]
-                continue
-            valid_ngram = False
-            while not valid_ngram:
-                options = self.getOptions(ngram)
-                (char, value) = options[index]
-                if value == 0:
-                    if len(ngram) > 0:
-                        ngram = ngram[1:]
-                        continue
-                    else:
-                        raise ValueError("no options.")
+    def decode(self, sequence):
+        text = ""
+        ngram = ""
+        for i, fraction in enumerate(sequence):
+            try:
+                if i % (len(sequence)//100) == 0:
+                    sys.stdout.write("\rMarkov decode {0:d}%".format(int(100*i/len(sequence))))
+                    sys.stdout.flush()
+            except ZeroDivisionError:
+                pass
 
-                text += char
-                ngram += char
-                if self.order > 0:
-                    ngram = ngram[-self.order:]
-                else:
-                    ngram = ""
-                valid_ngram = True
+            char = self.decode_char(fraction, ngram)
+
+            text += char
+            ngram += char
+            if self.order > 0:
+                ngram = ngram[-self.order:]
+            else:
+                ngram = ""
+        sys.stdout.write("\n")
+        sys.stdout.flush()
         return text
 
-    def getSequence(self, text):
+    def encode(self, text):
         sequence = []
         ngram = ""
         for i in range(len(text)):
             try:
                 if i % (len(text)//100) == 0:
-                    sys.stdout.write("\rMarkov get sequence %d%%" % int(100*i/len(text)))
+                    sys.stdout.write("\rMarkov encode {0:d}%".format(int(100*i/len(text))))
                     sys.stdout.flush()
             except ZeroDivisionError:
                 pass
 
-            valid_ngram = False
-            while not valid_ngram:
-                char = text[i]
-                options = self.getOptions(ngram)
-                index = list(map(lambda item: item[0], options)).index(char)
-                (_, value) = options[index]
-                if value == 0:
-                    if len(ngram) > 0:
-                        sequence.append(self._break_symbol)
-                        ngram = ngram[1:]
-                        continue
-                    else:
-                        raise ValueError("no options.")
+            char = text[i]
+            fraction = self.encode_char(char, ngram)
 
-                sequence.append(index)
-                ngram += char
-                if self.order > 0:
-                    ngram = ngram[-self.order:]
-                else:
-                    ngram = ""
-                valid_ngram = True
+            sequence.append(fraction)
+            ngram += char
+            if self.order > 0:
+                ngram = ngram[-self.order:]
+            else:
+                ngram = ""
+        sys.stdout.write("\n")
+        sys.stdout.flush()
         return sequence
+
+    def encode_char(self, char, ngram=""):
+        options = self.getOptions(ngram)
+        index = [option[0] for option in options].index(char)
+        fractions = [option[1] for option in options]
+        lower = sum(fractions[:index])
+        higher = lower + fractions[index]
+        return numpy.random.uniform(lower, higher, 1)[0]
+
+    def decode_char(self, value, ngram=""):
+        options = self.getOptions(ngram)
+        lower = 0
+        for (char, fraction) in options:
+            if value < lower + fraction:
+                break
+            else:
+                lower += fraction
+        return char
 
     def getOptions(self, ngram=""):
         options = []
-        for char in self._ordered_symbols:
+        missing = []
+        for char in self.symbols:
             key = ngram + char
             if key in self._data:
-                options.append((char, self._data[key]))
+                options.append((char, float(self._data[key])))
             else:
-                options.append((char, 0))
+                missing.append(char)
+
+        if len(missing) > 0:
+            sub_options = self.getOptions(ngram[1:])
+            sub_options = [option for option in sub_options if (option[0] in missing)]
+            sub_total = sum([option[1] for option in sub_options])
+            sub_options = [(char, 1.0*num/sub_total) for (char, num) in sub_options]
+            options = options + sub_options
+
+        total = sum([option[1] for option in options])
+        options = [(char, 1.0*num/total) for (char, num) in options]
+
         options.sort(key=lambda item: item[1], reverse=True)
         return options
--- TextGen_1.4/test.py
+++ TextGen_1.4/test.py
@@ -1,7 +1,5 @@
 import markov
-import huffman
 import utils
-import marsta
 
 import itertools
 import numpy
@@ -16,168 +14,25 @@ texts = utils.open_files(path, markov.Markov.symbols)
 files = list(texts.keys())
 random.shuffle(files)
 
-text_markov = "\n".join([texts[file] for file in files[:-5]])
-text_huffman = "\n".join([texts[file] for file in files[-5:-3]])
-text_huffman_second = "\n".join([texts[file] for file in files[-3:-1]])
-text_test = "\n".join([texts[file] for file in [files[-1]]])
+text_markov = "\n".join([texts[file] for file in files[:-2]])
+text_test = "\n".join([texts[file] for file in files[-2:]])
 
 if isfast:
     print("Fast training-----------------------")
     text_markov = text_markov[10000:110000]
-    text_huffman = text_huffman[10000:20000]
-    text_huffman_second = text_huffman_second[10000:20000]
     text_test = text_test[10000:11000]
-    width = 4
     order = 5
-    num = 100
 else:
     print("Slow training-----------------------")
     text_markov = text_markov
-    text_huffman = text_huffman
-    text_huffman_second = text_huffman_second[10000:500000]
     text_test = text_test[10000:20000]
-    width = 8
     order = 5
-    num = 10000
 
 mark = markov.Markov(text_markov, order=order)
+sequence = mark.encode(text_test)
+text2 = mark.decode(sequence)
+assert text2 == text_test
 
-training_sequence = mark.getSequence(text_huffman)
-huff = huffman.Huffman(training_sequence, list(range(len(mark.symbols) + 1)))
-
-training_sequence = mark.getSequence(text_huffman_second)
-training_data = huff.encode(training_sequence)
-training_data = [training_data[i:i + width] for i in range(0, width*(len(training_data)//width), width)]
-huff_second = huffman.Huffman(training_data, [''.join(bit) for bit in itertools.product(*(["01"]*width))])
-
-sequence = mark.getSequence(text_test)
-data = huff.encode(sequence)
-
-data = [data[i:i + width] for i in range(0, width*(len(data)//width), width)]
-data_second = huff_second.encode(data)
-data2 = huff_second.decode(data_second)
-data2 = "".join(data2)
-
-sequence2 = huff.decode(data2)
-text2 = mark.getText(sequence2)
-
-data = "".join(data)
-
-#print(text)
-_txt = ""
-for i in range(500):
-    if text_test[i] == " ":
-        _txt += "||"
-
-    _txt += repr(text_test[i]) + ", " + str(sequence[i]) + "|"
-
-    if text_test[i] == " ":
-        _txt += "||"
-print(_txt)
-#print(sequence)
-#print(data)
-#print(data_second)
-#print("decomp")
-#print(data2)
-#print(sequence2)
-#print(text2)
-assert (text2 in text_test)
-assert (",".join(map(str,sequence2)) in ",".join(map(str,sequence)))
-assert (data2 in "".join(data))
-
-## ---------------------CLEAN
-print("test data historical dependence with MarSta")
-print("sequence")
-tester = marsta.MarSta(sequence, order=1)
-tester_d = tester.allIndependent()
-for t in tester_d:
-    print(t)
-
-print("data")
-tester = marsta.MarSta(data, order=3)
-tester_d = tester.allIndependent()
-for t in tester_d:
-    print(t)
-
-print("data_second")
-tester = marsta.MarSta(data_second, order=3)
-tester_d = tester.allIndependent()
-for t in tester_d:
-    print(t)
-## ---------------------CLEAN
-
-print("data_second ", len(data_second), "bits.")
-print("data ", len(data2), "bits.")
-print("text ", len(text2)*numpy.log(len(mark.symbols))/numpy.log(2), "bits.")
-print("compression data_second ", len(data_second) / (len(text2)*numpy.log(len(mark.symbols))/numpy.log(2)), "percent")
-print("compression data ", len(data2) / (len(text2)*numpy.log(len(mark.symbols))/numpy.log(2)), "percent")
-
-tdata = [True if symbol=="1" else False for symbol in data]
-print("data moments")
-
-while len(tdata) > 100:
-    print(numpy.array(tdata).mean(), numpy.array(tdata).std()/numpy.sqrt(len(tdata) - 1))
-    tmp = []
-    for i in range(len(tdata)//2):
-        tmp.append(tdata[i*2]^tdata[1 + i*2])
-    tdata = tmp
-
-tdata = [True if symbol=="1" else False for symbol in data_second]
-print("data_second moments")
-
-while len(tdata) > 100:
-    print(numpy.array(tdata).mean(), numpy.array(tdata).std()/numpy.sqrt(len(tdata) - 1))
-    tmp = []
-    for i in range(len(tdata)//2):
-        tmp.append(tdata[i*2]^tdata[1 + i*2])
-    tdata = tmp
-
-print("\n\n\nTesting")
-import sys
-data = []
-sizes = []
-for i in range(num):
-    if i % (num//100) == 0:
-        sys.stdout.write("\r%d%%" % int(100*i/num))
-        sys.stdout.flush()
-
-    size = 500
-    new_data_second = "".join(map(str,numpy.random.random_integers(0, 1, size)))
-    new_data = huff_second.decode(new_data_second)
-    new_data = "".join(new_data)
-
-    new_sequence = huff.decode(new_data)
-    new_text = mark.getText(new_sequence)
-
-    data.append((len(new_text), new_text, new_data_second, new_data, new_sequence))
-    sizes.append(len(new_text))
-
-sizes.sort(reverse=True)
-data.sort(key=lambda node: node[0], reverse=True)
-print(numpy.array(sizes))
-
-print(data[0][1], ":text\n------------------------------\n")
-print(data[0][4], ":sequence\n")
-
-_txt = ""
-for i in range(len(data[0][1])):
-    if data[0][1][i] == " ":
-        _txt += "||"
-    _txt += data[0][1][i] + ", " + str(data[0][4][i]) + "|"
-    if data[0][1][i] == " ":
-        _txt += "||"
-print(_txt)
-
-print("bad\n")
-
-print(data[-1][1], ":text\n------------------------------\n")
-print(data[-1][4], ":sequence\n")
-
-_txt = ""
-for i in range(len(data[-1][1])):
-    if data[-1][1][i] == " ":
-        _txt += "||"
-    _txt += data[-1][1][i] + ", " + str(data[-1][4][i]) + "|"
-    if data[-1][1][i] == " ":
-        _txt += "||"
-print(_txt)
+sequence2 = numpy.random.uniform(0, 1.0, 1000)
+print(sequence2)
+print(mark.decode(sequence2))
